1b2c929e0857c43bb35baf85b3617eb99e4c0049
stsdev <sts.dev0488@gmail.com>
2018-03-20 11:35:37 +0800

    mock settings screen

--- App/Containers/Settings/index.js
+++ App/Containers/Settings/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { Text, View, AsyncStorage } from 'react-native'
+import { Text, View, AsyncStorage, Image, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { Content, Icon, Button } from 'native-base'
+import StarRating from 'react-native-star-rating'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from 'Redux/YourRedux'
 
 // Styles
 import styles from './styles'
+import { Images } from 'Themes/'
 
 class Settings extends Component {
   static navigationOptions = {
@@ -32,15 +34,92 @@ class Settings extends Component {
 
   render () {
     return (
-      <Content style={styles.container}>
-        <Text style={styles.titleText}>Settings</Text>
+      <View style={styles.container}>
+        <Content style={{flex: 1}}>
+          <View style={styles.titleSection}>
+            <Text style={styles.titleText}>Profile</Text>
+          </View>
 
-        <View style={{margin: 20}}>
-          <Button primary block bordered onPress={this._signOut}>
-            <Text>Logout</Text>
-          </Button>
+          <View style={styles.centered}>
+            <Image source={Images.launch} style={styles.logo} />
+          </View>
+
+          <View style={styles.section}>
+            <Text style={styles.subTitleText}>Joe Don</Text>
+            <Text style={styles.subTitleText}>Webart Contractor</Text>
+
+            <Text style={styles.sectionText}>Tampa, Florida</Text>
+            <Text style={styles.sectionText}>555 Generic Street</Text>
+            <Text style={styles.sectionText}>(813) 555-1313</Text>
+            <Text style={styles.sectionText}>www.webartcontractor.com</Text>
+            <Text style={styles.sectionText}>joe.don@john.com</Text>
+          </View>
+
+          <View style={[styles.section, styles.contactIcons]}>
+            <TouchableOpacity onPress={() => {}}>
+              <Icon name='md-call' style={styles.contactIcon} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => {}}>
+              <Icon name='md-text' style={styles.contactIcon} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => {}}>
+              <Icon name='md-mail' style={styles.contactIcon} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => {}}>
+              <Icon name='md-globe' style={styles.contactIcon} />
+            </TouchableOpacity>
+          </View>
+
+          <View style={[styles.section, styles.contactIcons, {justifyContent: 'center'}]}>
+            <TouchableOpacity onPress={() => {}}>
+              <Icon name='logo-facebook' style={styles.contactIcon} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => {}}>
+              <Icon name='logo-twitter' style={styles.contactIcon} />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.section}>
+            <Text style={styles.sectionText}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </Text>
+          </View>
+
+          <View style={styles.section}>
+            <Text style={styles.sectionText}>
+              Submitted 90 reviews
+            </Text>
+            <Text style={styles.sectionText}>
+              With an average rating of:
+            </Text>
+
+            <View style={{alignItems: 'center'}}>
+              <StarRating
+                disabled
+                starSize={20}
+                maxStars={5}
+                rating={3}
+                fullStarColor='#FFD700'
+                emptyStarColor='#D6D6D6'
+              />
+            </View>
+
+            <Text style={styles.sectionText}>
+              Clientell member since March 2015
+            </Text>
+          </View>
+        </Content>
+
+        <View style={styles.signOut}>
+          <TouchableOpacity onPress={() => this._signOut()}>
+            <Icon name='ios-power' style={{fontSize: 40}} />
+          </TouchableOpacity>
         </View>
-      </Content>
+      </View>
     )
   }
 }
--- App/Containers/Settings/styles.js
+++ App/Containers/Settings/styles.js
@@ -4,8 +4,34 @@ import { Colors, Metrics, ApplicationStyles } from 'Themes/'
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   container: {
-    flex: 1,
-    paddingTop: 40,
-    backgroundColor: Colors.background
+    flex: 1
+  },
+
+  logo: {
+    marginTop: 30,
+    height: 120,
+    width: 120,
+    resizeMode: 'contain'
+  },
+
+  centered: {
+    alignItems: 'center'
+  },
+
+  contactIcons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+
+  contactIcon: {
+    fontSize: 55,
+    marginLeft: 20
+  },
+
+  signOut: {
+    position: 'absolute',
+    top: 30,
+    right: 20
   }
 })
