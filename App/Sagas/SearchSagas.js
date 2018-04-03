93f112392d370e681a72606b5ba0ed9d3c8e9df8
stsdev <sts.dev0488@gmail.com>
2018-04-03 10:02:41 +0800

    add some catch errors on sagas

--- App/Sagas/SearchSagas.js
+++ App/Sagas/SearchSagas.js
@@ -3,25 +3,33 @@ import SearchActions from 'Redux/SearchRedux'
 import { apiGet, retryCall } from './StartupSagas'
 
 export function * getSearchResults ({ data }) {
-  const api = yield call(apiGet)
-  const response = yield call(api.clientLookup, data)
+  try {
+    const api = yield call(apiGet)
+    const response = yield call(api.clientLookup, data)
 
-  // success?
-  if (response.ok) {
-    yield put(SearchActions.searchSuccess(response.data))
-  } else {
-    yield put(SearchActions.searchFailure(response.data))
+    // success?
+    if (response.ok) {
+      yield put(SearchActions.searchSuccess(response.data))
+    } else {
+      yield put(SearchActions.searchFailure(response.data))
+    }
+  } catch (error) {
+    yield put(SearchActions.searchFailure({message: 'Search failed. Please try again.'}))
   }
 }
 
 export function * getFilteredClients ({ keyword }) {
-  const api = yield call(apiGet)
-  const response = yield call(api.clientFilter, keyword)
+  try {
+    const api = yield call(apiGet)
+    const response = yield call(api.clientFilter, keyword)
 
-  // success?
-  if (response.ok) {
-    yield put(SearchActions.clientFilterSuccess(response.data))
-  } else {
-    yield put(SearchActions.clientFilterFailure(response.data))
+    // success?
+    if (response.ok) {
+      yield put(SearchActions.clientFilterSuccess(response.data))
+    } else {
+      yield put(SearchActions.clientFilterFailure(response.data))
+    }
+  } catch (error) {
+    yield put(SearchActions.searchFailure({message: 'Search failed. Please try again.'}))
   }
 }
