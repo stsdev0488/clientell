aeab1296c6f48e874bbde3e8c787bec61a23b7ef
stsdev <sts.dev0488@gmail.com>
2019-08-27 17:43:09 +0800

    feat: add edit contact info link

--- App/Containers/Settings/CollapseContent/contactInfo.js
+++ App/Containers/Settings/CollapseContent/contactInfo.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { View } from 'react-native'
-import { Text } from 'native-base'
+import {Button, Text} from 'native-base'
 
 import styles from '../styles'
 
-export default ({ user }) => {
+export default ({ user, navigation }) => {
   return (
     <View style={styles.section}>
+      <View style={{flex: 1, alignItems: 'flex-end'}}>
+        <Button small transparent style={{alignSelf: 'flex-end'}} onPress={() => navigation.navigate('EditMyContactInfo', {user})}>
+          <Text>Edit</Text>
+        </Button>
+      </View>
+
       {user.company_name && <Text style={styles.subTitleText}>{user.company_name}</Text>}
 
       <Text style={styles.sectionText}>{`${user.city || ''}${user.state ? ', ' : ''}${user.state || ''} ${user.postal_code || ''}`}</Text>
